fa75e6863a264cf50c03c49c36eb3f042e5b31c0
samith <samithpiumal@gmail.com>
2017-02-15 14:14:21 +0530

    Initial prject createion

---
+++ src/main/java/com/sam/SamApplication.java
@@ -0,0 +1,12 @@
+package com.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamApplication.class, args);
+	}
+}
