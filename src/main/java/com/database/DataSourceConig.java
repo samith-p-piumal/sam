1fdd6a586c112e3e64650756a63bfb3deac76bd2
samith <samithpiumal@gmail.com>
2017-02-15 15:08:49 +0530

    Initial project run

---
+++ src/main/java/com/database/DataSourceConig.java
@@ -0,0 +1,7 @@
+package com.database;
+
+/**
+ * Created by samith_k on 2/15/2017.
+ */
+public class DataSourceConig {
+}
--- src/main/java/com/sam/SamApplication.java
+++ src/main/java/com/sam/SamApplication.java
@@ -1,12 +1,47 @@
-package com.sam;
 
+package com.sam;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamApplication.class, args);
 	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 }
+
+
+/*import org.springframework.boot.*;
+		import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.*;
+		import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class SamApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SamApplication.class, args);
+	}
+}*/
